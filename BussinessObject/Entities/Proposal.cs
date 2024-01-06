ba4bce20d24507dcb8f4a88ca6a1d3b1864b29bf
nhunht <nhtn0311@gmail.com>
2024-01-06 22:52:37 +0700

    Alow null expand field

--- BussinessObject/Entities/Proposal.cs
+++ BussinessObject/Entities/Proposal.cs
@@ -10,12 +10,12 @@ namespace BusinessObject.Entities
         public DateTime CreatedDate { get; set; }
         public DateTime? LastModifiedDate { get; set; }
         public string Status { get; set; } = null!;
-        public Guid RequirementId { get; set; }
+        public Guid? RequirementId { get; set; }
         public Guid? CreatedBy { get; set; }
         public Guid? ArtwordId { get; set; }
 
         public virtual Artwork? Artword { get; set; }
         public virtual Account? CreatedByNavigation { get; set; }
-        public virtual Requirement Requirement { get; set; } = null!;
+        public virtual Requirement? Requirement { get; set; };
     }
 }
