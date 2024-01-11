5d0c92667cbb4ca9cbdcb973404632d8301c322d
ThienNho <ThienNho@DESKTOP-BEJ8LO6>
2024-01-12 01:15:25 +0700

    add get role for a account

--- DataAccess/DAO/AccountDAO.cs
+++ DataAccess/DAO/AccountDAO.cs
@@ -42,6 +42,7 @@ namespace DataAccess.DAO
                     .Include(a => a.Rank)
                     .Include(a => a.AccountReviewAccounts).ThenInclude(ar => ar.CreatedByNavigation)
                     .Include(a => a.Artworks)
+                    .Include(a => a.AccountRoles).ThenInclude(ar => ar.Role)
                     .Include(a => a.Certificates)
                     .SingleOrDefault(a => a.Id == id)!;
             }
